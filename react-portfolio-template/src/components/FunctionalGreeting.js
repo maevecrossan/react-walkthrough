438c4df1282cf9771d9eff44bca56a60700ef7ef
Maeve Crossan <maevecrossan@gmail.com>
2025-01-10 18:20:32 +0000

    Set up react app and create functional greeting message.

---
+++ react-portfolio-template/src/components/FunctionalGreeting.js
@@ -0,0 +1,21 @@
+//commented out for clarity
+
+// import React from "react";
+
+// const FunctionalGreeting = () => <h1>Hello from react!</h1>
+
+// export default FunctionalGreeting 
+
+/* 
+exports the function so it can be imported and used elsewhere in our app
+*/
+
+/* 'default' allows you to import the function under a different name if necessary, 
+ despite the fact that it breaks convention.
+*/
+
+/* This function is STATELESS. 
+   - doesn't change
+   - no properties
+   - no ability to update itself in anyway
+*/
---
+++ react-portfolio-template/src/components/FunctionalGreetingWithProps.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const FunctionalGreetingWithProps = () => <h1>Hello!</h1>
+
+export default FunctionalGreetingWithProps 
+// exports the function so it can be imported and used elsewhere in our app
+
+/* 'default' allows you to import the function under a different name if necessary, 
+* despite the fact that it breaks convention.
+*/
+
+/* This functio
+*   - 
+*   - 
+*   - 
+*/
