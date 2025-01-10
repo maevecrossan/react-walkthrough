ac81d133d13189d8ce2e736589da507bc4b8d74f
Maeve Crossan <maevecrossan@gmail.com>
2025-01-10 18:21:21 +0000

    Remove old greeting function imports.

--- react-portfolio-template/src/App.js
+++ react-portfolio-template/src/App.js
@@ -1,12 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
-import FunctionalGreeting from './components/FunctionalGreeting';
 import FunctionalGreetingWithProps from './components/FunctionalGreetingWithProps';
 
 function App() {
   return (
     <div className="App">
-      <FunctionalGreeting />
       <FunctionalGreetingWithProps />
     </div>
   );
