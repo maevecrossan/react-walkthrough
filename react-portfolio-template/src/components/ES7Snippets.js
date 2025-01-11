f1a752e45d353911e5ab459b871a6d77d08712a5
Maeve Crossan <maevecrossan@gmail.com>
2025-01-11 16:59:09 +0000

    Add ES7 snippet notes

---
+++ react-portfolio-template/src/components/ES7Snippets.js
@@ -0,0 +1,67 @@
+// Notes only
+
+
+// ----------------------------------------------
+// ** rfce + tab: creates a function
+
+// import React from 'react'
+
+// function ES7Snippets() {
+//   return (
+//     <div>ES7Snippets</div>
+//   )
+// }
+
+// export default ES7Snippets
+
+
+// ----------------------------------------------
+// ** rafce + tab: arrow function
+
+// import React from 'react'
+
+// const ES7Snippets = () => {
+//   return (
+//     <div>ES7Snippets</div>
+//   )
+// }
+
+// export default ES7Snippets
+
+
+// ----------------------------------------------
+// ** rce + tab: class component
+
+// import React, { Component } from 'react'
+
+// export class ES7Snippets extends Component {
+//   render() {
+//     return (
+//       <div>ES7Snippets</div>
+//     )
+//   }
+// }
+
+// export default ES7Snippets
+
+
+// ----------------------------------------------
+// ** rconst + tab: class component
+
+// constructor(props) {
+//   super(props)
+
+//   this.state = {
+//      first
+//   }
+// }
+
+// ----------------------------------------------
+// ** clg + tab: console log object
+
+//console.log(first)
+
+// ----------------------------------------------
+// ** clg + tab: console log object and a string
+
+//console.log('first', first)
